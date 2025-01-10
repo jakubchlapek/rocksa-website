30090f39781a6a59c2cc974b34875618ebb02160
Jakub Chłapek <jakub.chlapekk@gmail.com>
2025-01-10 19:17:31 +0100

    Added basic welcome page

---
+++ rocksa/project/resources/views/components/welcome-container.blade.php
@@ -0,0 +1,6 @@
+<div class="flex flex-col w-full gap-[4rem] items-center">
+    <x-nav-bar class="flex justify-center"></x-nav-bar>
+    <div class="flex justify-center">
+        <p class="font-bold text-7xl invert text-center">Lepsze niż kobiety, te kamienie łupią się za darmo</p>
+    </div>
+</div>
