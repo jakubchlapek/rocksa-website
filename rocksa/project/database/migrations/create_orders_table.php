4ec3af8c71597a73022247ec38f0c6ac91f15832
Mikolaj Szuba <mikszuba@student.agh.edu.pl>
2025-01-14 19:09:29 +0100

    added timestamps, user_id

--- rocksa/project/database/migrations/create_orders_table.php
+++ rocksa/project/database/migrations/create_orders_table.php
@@ -15,6 +15,7 @@ return new class () extends Migration {
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id(); // Auto-incrementing ID
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('first_name'); // First name of the customer
             $table->string('last_name'); // Last name of the customer
             $table->string('street'); // Street address
@@ -22,7 +23,7 @@ return new class () extends Migration {
             $table->string('postal_code'); // Postal code
             $table->string('phone_number'); // Customer's phone number
             $table->string('email'); // Customer's email address
-            $table->timestamps(); // Created at & updated at timestamps
+            $table->timestamp(); // Created at & updated at timestamp
         });
     }
 
