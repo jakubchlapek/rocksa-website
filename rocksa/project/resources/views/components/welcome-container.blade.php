b1a534fcdaa48f0821c5e92345a665b706bf08f2
Jakub Chłapek <jakub.chlapekk@gmail.com>
2025-01-14 17:34:58 +0100

    Added main page animation (site might load too slow)

--- rocksa/project/resources/views/components/welcome-container.blade.php
+++ rocksa/project/resources/views/components/welcome-container.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col w-full gap-[4rem] items-center">
     <x-nav-bar class="flex justify-center"></x-nav-bar>
-    <div class="flex justify-center">
-        <p class="font-bold text-7xl invert text-center">Better than women, these rocks smash for free</p>
+    <div class="flex justify-center font-bold text-7xl invert text-center opacity-0 translate-y-4 animate-fade-in-up">
+        Better than women, these rocks smash for free
     </div>
 </div>
--- rocksa/project/tailwind.config.js
+++ rocksa/project/tailwind.config.js
@@ -14,6 +14,21 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            animation: {
+                'fade-in-up': 'fadeInUp 1s ease-out 0.5s forwards',
+            },
+            keyframes: {
+                fadeInUp: {
+                    '0%': {
+                        opacity: '0',
+                        transform: 'translateY(20px)',
+                    },
+                    '100%': {
+                        opacity: '1',
+                        transform: 'translateY(0)',
+                    },
+                },
+            },
         },
     },
 
