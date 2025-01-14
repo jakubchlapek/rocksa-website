cf35ac7d0955da0e3dc4fc40f443ecd87464fd1a
Mikolaj Szuba <mikszuba@student.agh.edu.pl>
2025-01-14 21:21:46 +0100

    order index update

--- rocksa/project/app/Http/Controllers/OrderController.php
+++ rocksa/project/app/Http/Controllers/OrderController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreOrderRequest;
@@ -7,13 +6,12 @@ use App\Http\Requests\UpdateOrderRequest;
 use App\Models\Order;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
-use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
     public function index(): View
     {
-        return view('orders.index')->with('orders', Order::where('user_id',auth()->id())->get());
+        return view('orders.index')->with('orders', Order::where('user_id', auth()->id())->get());
     }
 
     public function create(): View
@@ -24,7 +22,7 @@ class OrderController extends Controller
     public function store(StoreOrderRequest $request): RedirectResponse
     {
         $data = $request->validated();
-        $data['user_id'] = auth()->id();
+        $data['user_id'] = auth()->id(); // Dodajemy user_id dla zalogowanego użytkownika
         $order = Order::create($data);
 
         return redirect()->route('orders.show', $order);
@@ -32,16 +30,19 @@ class OrderController extends Controller
 
     public function show(Order $order): View
     {
+        $this->authorizeUser($order);
         return view('orders.show')->with('order', $order);
     }
 
     public function edit(Order $order): View
     {
+        $this->authorizeUser($order);
         return view('orders.edit')->with('order', $order);
     }
 
     public function update(UpdateOrderRequest $request, Order $order): RedirectResponse
     {
+        $this->authorizeUser($order);
         $order->update($request->validated());
 
         return redirect()->route('orders.show', $order);
@@ -49,8 +50,16 @@ class OrderController extends Controller
 
     public function destroy(Order $order): RedirectResponse
     {
+        $this->authorizeUser($order);
         $order->delete();
 
         return redirect()->route('orders.index');
+    }
+
+    private function authorizeUser(Order $order): void
+    {
+        if ($order->user_id !== auth()->id()) {
+            abort(403, 'Unauthorized action.');
+        }
     }
 }
