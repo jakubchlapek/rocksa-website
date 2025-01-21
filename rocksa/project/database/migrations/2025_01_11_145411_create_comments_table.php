8499dfb8f51a99e25de589e6eaee67247d5bc286
Mikolaj Szuba <mikszuba@student.agh.edu.pl>
2025-01-21 18:09:28 +0100

    selecting category in rock creator

--- rocksa/project/database/migrations/2025_01_11_145411_create_comments_table.php
+++ rocksa/project/database/migrations/2025_01_11_145411_create_comments_table.php
@@ -14,6 +14,7 @@ return new class () extends Migration {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('rock_id')->constrained()->onDelete('cascade');
+            $table->text('content');
             $table->string('title');
             $table->text('text');
             $table->timestamps();
